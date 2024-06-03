758d3dc885cf267d9df06e26dae80a24ceb82309
BarbaraGaito <ing.gaitobarbara@gmail.com>
2024-06-02 22:09:53 -0300

    Mejoras back

--- backend/miGanado/views.py
+++ backend/miGanado/views.py
@@ -1,8 +1,36 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Ganado
-from .serializers import GanadoSerializer  # Importa tu serializador
+from .models import Usuario, Lote, Animal, HistorialMedico, Tratamiento, Sangrado, Notificacion, ConfigNotificaciones
+from .serializers import UsuarioSerializer, LoteSerializer, AnimalSerializer, HistorialMedicoSerializer, TratamientoSerializer, SangradoSerializer, NotificacionSerializer, ConfigNotificacionesSerializer
 
-class GanadoViewSet(viewsets.ModelViewSet):
-    queryset = Ganado.objects.all()  # Especifica el queryset para la vista
-    serializer_class = GanadoSerializer  # Especifica el serializador para la vista
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class LoteViewSet(viewsets.ModelViewSet):
+    queryset = Lote.objects.all()
+    serializer_class = LoteSerializer
+
+class AnimalViewSet(viewsets.ModelViewSet):
+    queryset = Animal.objects.all()
+    serializer_class = AnimalSerializer
+
+class HistorialMedicoViewSet(viewsets.ModelViewSet):
+    queryset = HistorialMedico.objects.all()
+    serializer_class = HistorialMedicoSerializer
+
+class TratamientoViewSet(viewsets.ModelViewSet):
+    queryset = Tratamiento.objects.all()
+    serializer_class = TratamientoSerializer
+
+class SangradoViewSet(viewsets.ModelViewSet):
+    queryset = Sangrado.objects.all()
+    serializer_class = SangradoSerializer
+
+class NotificacionViewSet(viewsets.ModelViewSet):
+    queryset = Notificacion.objects.all()
+    serializer_class = NotificacionSerializer
+
+class ConfigNotificacionesViewSet(viewsets.ModelViewSet):
+    queryset = ConfigNotificaciones.objects.all()
+    serializer_class = ConfigNotificacionesSerializer
