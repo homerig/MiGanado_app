bb2dd547653789cf3bd0936c2b3cd74847754dc7
lara_alonso <laralonso@uade.edu.ar>
2024-07-15 20:41:52 -0300

    m

--- backend/mi_ganado_backend/settings.py
+++ backend/mi_ganado_backend/settings.py
@@ -54,7 +54,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://192.168.0.182:8081",  
     "http://192.168.0.10:8081",
     "http://192.168.0.209:8081",
-    "htpp://192.168.0.181:8081",
+    "http://192.168.0.181:8081",
 ]
 
 ROOT_URLCONF = 'mi_ganado_backend.urls'
